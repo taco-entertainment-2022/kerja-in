0bd6992175ec79a0aa6d54f57fd94ddb7ad3e8bf
Zidan Ramadhan <zidan.ramadhan@ui.ac.id>
2022-10-19 09:48:04 +0700

    Add shadow and initial details

--- kerja-in/Model/JobModel.swift
+++ kerja-in/Model/JobModel.swift
@@ -11,17 +11,22 @@ import UIKit
 class JobModel {
     var userImage: UIImage?
     var jobName: String?
+    var posted: String?
+    var userName: String?
     var date: String?
+    var duration: String?
     var location: String?
     var price: String?
-    var posted: String?
     
-    init(userImage: UIImage, jobName: String, date: String, location: String, price: String, posted: String) {
+    init(userImage: UIImage, jobName: String, posted: String, userName: String, date: String, duration: String, location: String, price: String) {
         self.userImage = userImage
         self.jobName = jobName
+        self.posted = posted
+        self.userName = userName
         self.date = date
+        self.duration = duration
         self.location = location
         self.price = price
-        self.posted = posted
+        
     }
 }
