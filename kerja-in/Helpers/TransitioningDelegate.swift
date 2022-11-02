673df3edae5f2786a89d4e04ff5554122749c00e
Sherary <sherary.apriliana@gmail.com>
2022-11-03 02:24:27 +0700

    add hover half modal

---
+++ kerja-in/Helpers/TransitioningDelegate.swift
@@ -0,0 +1,36 @@
+//
+//  TransitioningDelegate.swift
+//  kerja-in
+//
+//  Created by Sherary Apriliana on 03/11/22.
+//
+
+import UIKit
+
+class TransitioningDelegate: NSObject, UIViewControllerTransitioningDelegate {
+    func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
+        return PresentationController(presentedViewController: presented, presenting: presenting)
+    }
+}
+
+class PresentationController: UIPresentationController {
+    override var frameOfPresentedViewInContainerView: CGRect {
+        let bounds = presentingViewController.view.bounds
+        let size = CGSize(width: 390, height: 300)
+        let origin = CGPoint(x: bounds.midX - size.width / 2, y: bounds.midY - size.height / 2)
+        return CGRect(origin: origin, size: size)
+    }
+    
+    override init(presentedViewController: UIViewController, presenting presentingViewController: UIViewController?) {
+        super.init(presentedViewController: presentedViewController, presenting: presentingViewController)
+        
+        presentedView?.autoresizingMask = [
+            .flexibleTopMargin,
+            .flexibleBottomMargin,
+            .flexibleLeftMargin,
+            .flexibleRightMargin,
+        ]
+        
+        presentedView?.translatesAutoresizingMaskIntoConstraints = true
+    }
+}
