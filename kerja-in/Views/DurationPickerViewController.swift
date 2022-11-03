ac04d5228b632d097242db8d0a0dfe6d47e74ae4
Sherary <sherary.apriliana@gmail.com>
2022-11-03 10:32:48 +0700

    reversing half modal

--- kerja-in/Views/DurationPickerViewController.swift
+++ kerja-in/Views/DurationPickerViewController.swift
@@ -8,10 +8,50 @@
 import UIKit
 
 class DurationPickerViewController: UIViewController {
+    
+    let numPicker = Array(1...60)
+    let durationPicker = ["Menit", "Jam", "Hari", "Bulan", "Tahun"]
+    
+    private lazy var durationPickerView: UIPickerView = {
+        let pickerView = UIPickerView()
+        pickerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return pickerView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        durationPickerView.delegate = self
+        durationPickerView.dataSource = self
+    }
+}
+
+extension DurationPickerViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == 0 {
+            return numPicker.count
+        }
+        
+        return durationPicker.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if component == 0 {
+            return "\(numPicker[row])"
+        }
+        
+        return "\(durationPicker[row])"
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let numSelected = numPicker[pickerView.selectedRow(inComponent: 0)]
+        let durationSelected = durationPicker[pickerView.selectedRow(inComponent: 1)]
+        
+        print(numSelected, durationSelected)
     }
 }
